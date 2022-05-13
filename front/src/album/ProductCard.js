99d8a46777e7d3c5e3b5bc646eac30487d8333b7
trabelsimahdi <77326924+trabelsimahdi@users.noreply.github.com>
2022-05-13 01:03:29 +0100

    A

--- front/src/album/ProductCard.js
+++ front/src/album/ProductCard.js
@@ -35,11 +35,11 @@ function Item(props) {
   return (
     <Box
       sx={{
-        p: 1,
-        m: 1,
+        p: 0.3,
+        m: 0.3,
         bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : 'grey.100'),
         color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
-        border: '0.4px solid',
+        border: '1px solid',
         borderColor: (theme) =>
           theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
         borderRadius: 2,
